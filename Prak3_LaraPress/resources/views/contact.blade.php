8396e37cf2909f5f254b2d6c37313c1259132cf5
Ghifar jnr <ghifar4523051@univpancasila.ac.id>
2025-10-03 21:02:26 +0700

    Add Prak3_LaraPress

---
+++ Prak3_LaraPress/resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak Saya - GhifarPress</title>
+</head>
+<body>
+    <h1>Kontak GhifarPress</h1>
+    <p>Hubungi Kami segera</p>
+    
+    <h2>Informasi Kontak</h2>
+    <p>Jika Anda memiliki pertanyaan atau ingin bekerja sama, silakan hubungi kami melalui salah satu cara berikut:</p>
+    
+    <ul>
+        <li><strong>Telepon  :</strong> <a href="tel: ‪+62211334455‬">‪+62 21 1234 567‬</a></li>
+        <li><strong>WhatsApp :</strong> <a href="https://wa.me/628123456789">‪+62 81 2345 6789‬</a></li>
+        <li><strong>Email    :</strong> <a href="mailto:kontak@ghifarpress.id">larapres@gmail.com</a></li>
+    </ul>
+    
+    <hr>
+    <a href="/">Lihat Halaman Utama</a>
+    <br>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+
+</body>
+</html>
