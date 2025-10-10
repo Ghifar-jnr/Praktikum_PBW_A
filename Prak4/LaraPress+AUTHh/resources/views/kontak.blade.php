aa3b34420401f00811089468fb4cb8269b4c05b2
divacahya <spodiva@gmail.com>
2025-10-10 19:11:44 +0700

    Pertemuan 4

---
+++ Prak4/LaraPress+AUTHh/resources/views/kontak.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>kontak - larapress</title>
+</head>
+<body>
+    <h1>Selamat datang di kontak Larapress</h1>
+    <p>Ini adalah halaman kontak dari aplikasi blog kita.</p>
+    <a href="/tentang-kami">Lihat halaman tentang kami</a>
+    <br>
+    <br>
+    <a href="/">Kembali ke halaman utama</a>
+</body>
+</html>
---
+++ Prak4/LaraPress+AUTHh/resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Selamat datang di larapress</title>
+</head>
+<body>
+    <h1>Selamat datang di Blog Larapress</h1>
+    <p>Ini adalah halaman utama dari aplikasi blog kita.</p>
+    <a href="/tentang-kami">Lihat halaman tentang kami</a>
+    <br>
+    <br>
+    <a href="/kontak">Lihat halaman kontak kami</a>
+    <br>
+    <br>
+    <a href="/login">LOGIN</a>
+    <br>
+    <br>
+    <a href="/register">RESGISTER</a>
+</body>
+</html>
